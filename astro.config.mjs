78af7da3c35cf0ecdde7ae374b26a495efc5a4c7
oephi <r.sickle@protonmail.com>
2026-08-18 15:41:57 +1000

    remove react and keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
 import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://tiny-lore.vercel.app',
-  integrations: [react(), keystatic(), sitemap()],
+  integrations: [sitemap()],
   output: 'static',
   adapter: vercel(),
 });
